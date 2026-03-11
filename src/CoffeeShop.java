fec1d317e50d33737361868e553e59ab63d7dc64
GHOST <s68122250060@ssru.ac.th>
2026-03-11 21:17:02 +0700

    first commit

---
+++ src/CoffeeShop.java
@@ -0,0 +1,14 @@
+public class CoffeeShop {
+public static void main(String[] args) {
+
+Thread t1 = new Thread(new CoffeeMaker("Espresso"));
+Thread t2 = new Thread(new CoffeeMaker("Latte"));
+Thread t3 = new Thread(new CoffeeMaker("Cappuccino"));
+
+t1.start();
+t2.start();
+t3.start();
+
+System.out.println("🛎 รับออเดอร์ครบแล้ว");
+}
+}
